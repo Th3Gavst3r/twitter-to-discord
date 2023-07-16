8100fbee24272c11a2774269b13205cb1b38e331
Th3Gavst3r <th3gavst3r@gmail.com>
2023-07-16 00:34:54 -0400

    Use logging framework

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pprint import pprint
 import requests
+import logging
 
 from cloudevents.http import CloudEvent
 import functions_framework
@@ -8,15 +9,20 @@ from firebase_admin import firestore
 
 from nitter_scraper import get_tweets
 
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
 # Application Default credentials are automatically created.
 app = firebase_admin.initialize_app()
 db = firestore.client()
 
 def twitter_to_discord(data, context=None) -> str:
+    logging.debug('Received invocation')
     destinations = db.collection('destinations').stream()
 
     for destination_snapshot in destinations:
+        logging.debug(f'Processing destination {destination_snapshot.id}')
+
         destination = destination_snapshot.to_dict()
         is_updated = False
 
@@ -26,7 +32,12 @@ def twitter_to_discord(data, context=None) -> str:
 
         for user in destination['users']:
             for tweet in get_tweets(user['username'], pages=5):
+            logging.debug(f'Processing user {user["username"]}')
+
+                logging.debug(f'Processing tweet {tweet.tweet_id}')
+
                 if ('disable_retweets' in user and user['disable_retweets'] and tweet.is_retweet):
+                    logging.info(f'Ignoring {user["username"]}\'s retweet of {tweet.tweet_id}')
                     continue
 
                 if ('tweets' not in destination):
@@ -38,14 +49,15 @@ def twitter_to_discord(data, context=None) -> str:
                         'content': tweet_url
                     }
 
-                    print(f'Posting new tweet: {tweet_url}')
+                    logging.info(f'Posting new tweet: {tweet_url}')
                     requests.post(destination['webhook_url'], body)
 
                     destination['tweets'].append(tweet.dict())
                     is_updated = True
 
         if is_updated:
+            logging.info(f'Updating document for destination {destination_snapshot.id}')
             db.collection('destinations').document(destination_snapshot.id).set(destination)
 
-    print('Tweets refreshed')
+    logging.info('Tweets refreshed')
     return 'Tweets refreshed'
